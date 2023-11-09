7be4e2c3d829e0e958230a69e3c8b1f2679ab03d
Andrei Filip <andreifilip123@gmail.com>
2023-11-10 00:41:26 +0200

    Add basic separation

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import demucs.separate
+from os.path import join
+
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
+    path = join('data', 'file.mp4')
+    demucs.separate.main(["--mp3", "--two-stems", "vocals", path])
     return "<p>Hello, World!</p>"

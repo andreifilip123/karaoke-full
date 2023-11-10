c8172d6e994e38c10a9594e83e986a90dd33b056
Andrei Filip <andreifilip123@gmail.com>
2023-11-10 12:45:59 +0200

    Add song separation functionality to API endpoint with time

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_rq2 import RQ
 import demucs.separate
 from os.path import join
+import time
 
 app = Flask(__name__)
+
+def separate_song_parts(path, model='htdemucs', jobs=1):
+    demucs.separate.main(['-n',model, '--mp3', '-j', jobs, '--two-stems', 'vocals', path])
 
 @app.route("/api/python")
 def hello_world():
     path = join('data', 'file.mp4')
-    demucs.separate.main(["--mp3", "--two-stems", "vocals", path])
-    return "<p>Hello, World!</p>"
+    start_time = time.time()
+    separate_song_parts(path)
+    seconds = time.time() - start_time
+    minutes = seconds / 60
+    return f"Separation took {minutes:.2f} minutes"

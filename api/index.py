872ecb81d93f2afa4196831321c541796b98a691
Andrei Filip <andreifilip123@gmail.com>
2023-11-10 13:49:13 +0200

    Add job queue functionality to separate_song_parts function

--- api/index.py
+++ api/index.py
@@ -5,15 +5,27 @@ from os.path import join
 import time
 
 app = Flask(__name__)
+rq = RQ(app)
 
-def separate_song_parts(path, model='htdemucs', jobs=1):
-    demucs.separate.main(['-n',model, '--mp3', '-j', jobs, '--two-stems', 'vocals', path])
+
+@rq.job
+def separate_song_parts(path, model="htdemucs", jobs=1):
+    demucs.separate.main(
+        ["-n", model, "--mp3", "-j", jobs, "--two-stems", "vocals", path]
+    )
+
 
 @app.route("/api/python")
 def hello_world():
-    path = join('data', 'file.mp4')
-    start_time = time.time()
-    separate_song_parts(path)
-    seconds = time.time() - start_time
-    minutes = seconds / 60
-    return f"Separation took {minutes:.2f} minutes"
+    path = join("data", "file.mp4")
+    job = separate_song_parts.queue(path)
+    status = job.get_status()
+
+    return '<a href="/api/jobs/' + job.id + '/status">' + status + "</a>"
+
+@app.route("/api/jobs/<job_id>/status")
+def job_status(job_id):
+    job = rq.get_queue().fetch_job(job_id)
+    if job is None:
+        return "No such job", 404
+    return job.get_status()

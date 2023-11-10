c79d05e6a3b74b14e89feb67907cbe38c7e57844
Andrei Filip <andreifilip123@gmail.com>
2023-11-10 14:15:08 +0200

    Fix file path in separate_song_parts function

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ rq = RQ(app)
 @rq.job
 def separate_song_parts(path, model="htdemucs", jobs='1'):
     demucs.separate.main(
-        ["-n", model, "--mp3", "-j", jobs, "--two-stems", "vocals", path]
+        ["-n", model, "--mp3", "-j", jobs, "--two-stems", "vocals", join("../", path)]
     )
 
 
